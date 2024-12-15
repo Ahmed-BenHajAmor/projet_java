b51c44478ccba3fbf08757558b798bd503b55c43
Ahmed <ahmed.benhajamorr@gmail.com>
2024-12-15 09:31:33 +0100

    adding comments

--- src/main/java/com/projet/ConnectionBD/ConnectionBD.java
+++ src/main/java/com/projet/ConnectionBD/ConnectionBD.java
@@ -5,9 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
+
 public class ConnectionBD {
-    public static Connection con;
-    public static Statement st;
+    public static Connection con; // Objet Connection pour gérer la connexion à la base de données.
+    public static Statement st;// Objet Statement pour exécuter les requêtes SQL.
+
     public static void init() throws ClassNotFoundException {
         try {
             String url = "jdbc:mysql://localhost:3306/plateformedeformation";
@@ -15,11 +18,14 @@ public class ConnectionBD {
             con = DriverManager.getConnection(url, "root", "Promysql001/");
             st = con.createStatement();
         } catch (SQLException e) {
-            System.out.println("connection echoué");
+            // Gestion des erreurs de connexion
+            System.out.println("connection echouée");
         }
         
     }
+
     public static void close() throws SQLException{
+        // Fermeture des objets Statement et Connection pour libérer les ressources
         st.close();
         con.close();
     }
--- src/main/java/com/projet/communicationBD/communicationBD.java
+++ src/main/java/com/projet/communicationBD/communicationBD.java
@@ -9,29 +9,35 @@ public class communicationBD {
 
 
     public static int insert(String table, String[] fields, Object[] values) throws SQLException{
+        // Construction de la chaîne de colonnes au format SQL : (colonne1, colonne2, ...)
         String fieldsString = "(";
         for(int i = 0; i<fields.length-1; i++){
             fieldsString += fields[i] + ", ";
         }
         fieldsString += fields[fields.length-1]+")";
 
+        // Construction de la chaîne de marqueurs de position : (?, ?, ...)
         String valuesQuestionMarkString = "(";
         for(int i = 0; i<values.length-1; i++){
             valuesQuestionMarkString += "?,";
         }
         valuesQuestionMarkString += "?)";
 
+        // Préparation de la requête SQL avec des marqueurs de position pour les valeurs
         PreparedStatement pst = ConnectionBD.con.prepareStatement("insert into "+table+" "+fieldsString+" values"+valuesQuestionMarkString);
         for(int i = 0; i<values.length; i++){
             Object value = values[i];
             if (value instanceof String) {
-                pst.setString(i + 1, (String) value);
+                pst.setString(i + 1, (String) value); // Assignation d'une chaîne de caractères
             } else if (value instanceof Integer) {
-                pst.setInt(i + 1, (Integer) value);
+                pst.setInt(i + 1, (Integer) value); // Assignation d'un entier
             } else if (value instanceof Double) {
-                pst.setDouble(i + 1, (Double) value);
+                pst.setDouble(i + 1, (Double) value); // Assignation d'un nombre reel
             }
         }
+
+
+        // Exécution de la requête SQL et récupération du nombre de lignes affectées
         int changesNumber = pst.executeUpdate();
         return changesNumber;
     }
